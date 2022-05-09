1f5c3de7a52545b33fe0cfd4a92614bee1d40bc1
Robert Sheffield <sheffira2@gmail.com>
2022-05-09 15:13:57 -0700

    added perlin noise class, block textures

--- main.py
+++ main.py
@@ -4,18 +4,48 @@ from mesh_terrain import MeshTerrain
 
 app = Ursina()
 
-window.color = color.rgb(200, 0, 255)
+window.color = color.rgb(0, 200, 255)
+indra = Sky()
+indra.color = window.color
 subject = FirstPersonController()
+
+subject.gravity = 0
 
 terrain = MeshTerrain()
 
 terrain.genTerrain()
 
 def update():
-    pass
+    block_found = False
+    
+    # Step up or down
+    step = 2
+    height = 1.86
+    
+    x = str(floor(subject.x + .5))
+    z = str(floor(subject.z + .5))
+    y = floor(subject.y + .5)
+    
+    for i in range(-step, step):
+        location = f'x{x}y{str(y + i)}z{z}'
+        if terrain.terrain_dict.get(location):
+            target = y + i + height
+            block_found = True
+            break
 
+            
+    if block_found:
+        subject.y = lerp(subject.y, target, 6 * time.dt)
+    else:
+        subject.y -= 9.8 * time.dt
+        
+    # BUTTON PRESSES
+
+        
+        
 def input(key):
     if key == 'escape':
         quit()
+        
 
 app.run()
--- mesh_terrain.py
+++ mesh_terrain.py
@@ -1,6 +1,7 @@
 from re import S, U
 from perlin_noise import PerlinNoise
 from ursina import Entity, load_model, load_texture, Mesh, Vec3, Vec2
+from perlin import Perlin
 from math import floor
 
 
@@ -8,15 +9,17 @@ class MeshTerrain:
     def __init__(self):
         self.subsets = []
         self.num_subsets = 1
-        self.subwidth = 32
+        self.subwidth = 64
         self.block = load_model('block.obj')
         self.textureAtlas = load_texture('texture_atlas_3.png')
-        self.perlin_noise = PerlinNoise(octaves=2, seed=20)
+        self.perlin_noise = Perlin()
         self.freq = 100
         self.amp = 20
+        self.terrain_dict = {}
+        
         
         for i in range(self.num_subsets):
-            e = Entity(model=Mesh(), texture="white_cube")
+            e = Entity(model=Mesh(), texture=self.textureAtlas)
             e.texture_scale *= 64 / e.texture.width
             self.subsets.append(e)
 
@@ -32,9 +35,18 @@ class MeshTerrain:
         # coordinates for grass in the texture atlas
         uu = 8
         uv = 7
+        
+        print(y)
+        if y > -5:
+            uu = 8
+            uv = 6
+            
         model.uvs.extend([Vec2(uu, uv) + u for u in self.block.uvs])
         
-        model.generate()
+        
+        # record terrain in dictionary
+        self.terrain_dict[f'x{floor(x)}y{floor(y)}z{floor(z)}'] = True
+        
         
     def genTerrain(self):
         x = 0
@@ -44,7 +56,7 @@ class MeshTerrain:
         for i in range(self.subwidth * self.subwidth):
             x = floor(i / self.subwidth)
             z = floor(i % self.subwidth)
-            y = floor(self.perlin_noise([x/self.freq, z/self.freq]) * self.amp)
+            y = self.perlin_noise.get_height(x, z)
             self.genBlock(x, y, z)
     
-        
+        self.subsets[0].model.generate()
